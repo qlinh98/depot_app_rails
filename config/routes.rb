a87681c9f9eab71f20410d79f80e5783b9e2ff61
Linh Ruby <qlinh1080@gmail.com>
2020-03-18 17:28:30 +0700

    17_28

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'store/index'
+  root 'store#index'
   resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
