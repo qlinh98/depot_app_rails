3fed03a3836e3d7d0e513b9238b12081ac676c0c
qlinh98 <qlinh1080@gmail.com>
2020-03-24 17:34:20 +0700

    Task J

--- config/routes.rb
+++ config/routes.rb
@@ -7,12 +7,14 @@ Rails.application.routes.draw do
     delete "logout" => :destroy
   end
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root "store#index", as: "store_index"
   resources :products do
     get :who_bought, on: :member
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
+  scope "(:locale)" do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: "store_index", via: :all
   end
 end
